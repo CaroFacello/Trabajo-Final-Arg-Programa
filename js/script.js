4f1a9bd0a9f7583cc133b0c0bfe8f6e7bfb198b8
CaroFacello <carofacello.13@gmail.com>
2023-04-14 16:41:39 -0300

    subir casi ultimo

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+const formularioUsuarioHTML = document.getElementById("alerta-enviado");
+
+formularioUsuarioHTML.addEventListener("submit", (event) =>{
+    event.preventDefault();
+  alert("Tu mensaje se envió con éxito");   })
+
