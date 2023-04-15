75e5e507094d26ce86e34b6c23f0356fc3a3e158
Yanel25 <129886333+Yanel25@users.noreply.github.com>
2023-04-14 22:59:34 -0300

    Add files via upload
    Final lading page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 
+function validar(){
+    const nombre=document.getElementById("nombre").value;
+    
+    const email=document.getElementById("email").value;
 
-const formularioUsuarioHTML = document.getElementById("alerta-enviado");
-
-formularioUsuarioHTML.addEventListener("submit", (event) =>{
-    event.preventDefault();
-  alert("Tu mensaje se envió con éxito");   })
-
+    const telefono=document.getElementById("telefono").value;
+    
+    if(nombre.length>0 && telefono>0 && email.length>0){
+        console.log("Mensaje enviado con exito");
+        alert("Mensaje enviado con exito");   
+    } 
+    else{
+        console.log("Mensaje no enviado. Por favor completar los datos correspondientes");
+        alert("Mensaje no enviado. Por favor completar los datos correspondientes");   
+    }
+}
